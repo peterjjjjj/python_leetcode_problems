e4be679b4c90b9fd2e8e6b1c146a80e113253e07
mengfeijin <jin.xzy@outlook.com>
2025-04-24 16:41:53 -0700

    validAnagram_0

---
+++ validAnagram.py
@@ -0,0 +1,23 @@
+def isAnagram_dict(str1, str2):
+    str1_dict = {}
+    str2_dict = {}
+    for letter in str1:
+        if letter not in str1_dict:
+            str1_dict[letter] = 1
+        elif letter in str1_dict:
+            str1_dict[letter] += 1
+
+    for letter in str2:
+        if letter not in str2_dict:
+            str2_dict[letter] = 1
+        elif letter in str2_dict:
+            str2_dict[letter] += 1
+
+    if len(str1_dict) != len(str2_dict):
+        return False
+    else :
+
+        return True
+
+if __name__ == '__main__':
+    print(isAnagram_dict("abba", "abba"))
