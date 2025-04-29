923301bf14577df04fc98b5c35fb9865dc5b423a
mengfeijin <jin.xzy@outlook.com>
2025-04-29 12:04:21 -0700

    2SumArray_0

---
+++ 2SumArray.py
@@ -0,0 +1,16 @@
+def twoSum(nums, target):
+    i = 0
+    j = len(nums)-1
+    while i < j:
+        if nums[i] + nums[j] == target:
+            return [i+1, j+1]
+        elif nums[i] + nums[j] > target:
+            j -= 1
+            continue
+        elif nums[i] + nums[j] < target:
+            i += 1
+            continue
+        return False
+
+if __name__ == '__main__':
+    print(twoSum([2,7,11,15], 9))
