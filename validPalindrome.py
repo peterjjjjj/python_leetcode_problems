eb09f03eb6c47a125d2d69d3e38646aa3ae0a3a2
mengfeijin <jin.xzy@outlook.com>
2025-04-29 11:51:51 -0700

    validPalindrome_1

--- validPalindrome.py
+++ validPalindrome.py
@@ -1,14 +1,18 @@
 def isPalindrome(s):
+    """"
+    O(n)
+    O(n)
+    """
     s = s.lower()
     s_plain = ''
-    for i in range(len(s) - 1):
-        if s[i].isalpha():
+    for i in range(len(s)):
+        if s[i].isalpha() or s[i].isdigit():
             s_plain += s[i]
-    for i in range((len(s_plain) - 1)//2):
-        if s_plain[i] == s_plain[-i]:
+    for i in range((len(s_plain))//2):
+        if s_plain[i] == s_plain[len(s_plain) - i - 1]:
             continue
         return False
     return True
 
 if __name__ == '__main__':
-    print(isPalindrome("amA"))
+    print(isPalindrome("0P"))
