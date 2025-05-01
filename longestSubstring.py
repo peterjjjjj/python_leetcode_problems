0371a4939c7babd59c85d091c283def675a2220c
mengfeijin <jin.xzy@outlook.com>
2025-05-01 15:18:31 -0700

    longestSubstring_chatgptVersion

--- longestSubstring.py
+++ longestSubstring.py
@@ -1,32 +1,26 @@
 def longestSubstring(string):
-    left = 0
-    right = 0
-    length = 0
-    current_substring = []
-    substrings = []
-    substring_length = 0
-    for i in range(len(string)):
-        if string[i] not in current_substring:
-            right = i
-            current_substring.append(string[i])
-            length += 1
-            continue
+    left_index = 0
+    char_set = set()
+    max_length = 0
+    result_start_index = 0
 
-        if length > substring_length:
-            substrings = (string[left:right+1])
-            left = i
-            current_substring = []
-            current_substring.append(string[i])
-            substring_length = length
-            length = 1
+    for right_index in range(len(string)):
+        current_char = string[right_index]
 
+        #remove all items before index
+        while current_char in char_set:
+            char_set.remove(string[left_index])
+            left_index += 1
 
+        char_set.add(current_char)
 
-    if length > substring_length:
-        substrings = (string[left:right + 1])
-        substring_length = length
+        current_max_length = right_index + 1 - left_index
+        if current_max_length > max_length:
+            max_length = current_max_length
+            result_start_index = left_index
 
-    return substrings
+    return string[result_start_index : result_start_index + max_length]
+
 
 if __name__ == '__main__':
-    print(longestSubstring('aab'))
+    print(longestSubstring('dvdfe'))
