eec4ec5de3183e0e53e37d590ae508f7513e20e2
mengfeijin <jin.xzy@outlook.com>
2025-05-03 20:09:38 -0700

    reverse_linked_list_gptversion_1

--- easy/revserse_linked_list.py
+++ easy/revserse_linked_list.py
@@ -5,11 +5,15 @@ class ListNode(object):
 
 class Solution(object):
     def reverseList(self, head):
+        #basecase when reached the tail
         if not head or not head.next:
             return head
 
+        #new_head is the tail
         new_head = self.reverseList(head.next)
+        #head is the node before tail, tail.next is now pointing towards the head before it
         head.next.next = head
+
         head.next = None
         return new_head
 
