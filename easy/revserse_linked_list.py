44c0809babdd21108e2f67f2f246d14e6a192828
mengfeijin <jin.xzy@outlook.com>
2025-05-03 19:29:24 -0700

    reverse_linked_list_gptversion

---
+++ easy/revserse_linked_list.py
@@ -0,0 +1,15 @@
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def reverseList(self, head):
+        if not head or not head.next:
+            return head
+
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return new_head
+
