1036557ee95bd4fc470b544e50c54801f721e269
mengfeijin <jin.xzy@outlook.com>
2025-05-05 00:05:36 -0700

    group_anagram_1

--- medium/group_anagram.py
+++ medium/group_anagram.py
@@ -1,1 +1,15 @@
-def group_anagrams(word):
+def group_anagrams(words):
+    groups = {}
+    for word in words:
+        sorted_word = sorted(word)
+        sorted_word = "".join(sorted_word)
+
+        if sorted_word not in groups:
+            groups[sorted_word] = [word]
+        else:
+            groups[sorted_word].append(word)
+
+    return list(groups.values())
+
+if __name__ == '__main__':
+    print(group_anagrams(["eat","tea","tan","ate","nat","bat"]))
