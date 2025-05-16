1a57ee651443ccfb3672f652d4e75bbd65b01c50
mengfeijin <jin.xzy@outlook.com>
2025-05-15 19:22:13 -0700

    longest_repeating_char_replacement_1_still little confused but okay

--- medium/longest_repeating_char_replacement.py
+++ medium/longest_repeating_char_replacement.py
@@ -1,4 +1,15 @@
 def longest_repeating_char_replacement(s, k):
+
+    """
+
+    :param s:
+    :param k:
+    :return:
+
+    tc = O(n)
+    sc = O(n)
+    """
+
     current_char = dict()
     left = 0
     right = 0
@@ -15,18 +26,14 @@ def longest_repeating_char_replacement(s, k):
 
         if current_char[s[i]] >= current_max_freq:
             current_max_freq = current_char[s[i]]
-            if right - left + 1 - current_max_freq > k:
-                while right - left + 1 - current_max_freq > k:
-                    current_char[s[left]] -= 1
-                    if s[left] == s[i]:
-                        current_max_freq -= 1
-                    left += 1
 
-                max_freq = max(max_freq, right - left + 1)
+        while right - left + 1 - current_max_freq > k:
+            current_char[s[left]] -= 1
+            left += 1
 
-
+        max_freq = max(right - left + 1, max_freq)
 
     return max_freq
 
 if __name__ == '__main__':
-    print(longest_repeating_char_replacement('ABAA', 0))
+    print(longest_repeating_char_replacement('BADEB', 2))
