9eca7a09ede0ff9b07d3eff184820f9817678e9d
mengfeijin <jin.xzy@outlook.com>
2025-05-16 00:09:13 -0700

    some comments

--- medium/longest_repeating_char_replacement.py
+++ medium/longest_repeating_char_replacement.py
@@ -8,6 +8,12 @@ def longest_repeating_char_replacement(s, k):
 
     tc = O(n)
     sc = O(n)
+
+    left = pointer to the left bound
+    right = pointer to the right bound
+
+
+
     """
 
     current_char = dict()
@@ -27,10 +33,14 @@ def longest_repeating_char_replacement(s, k):
         if current_char[s[i]] >= current_max_freq:
             current_max_freq = current_char[s[i]]
 
+
+        #right - left + 1 indicates the current substring, right - left + 1 - current_max_freq is the number of chars need to be replaced
         while right - left + 1 - current_max_freq > k:
+            #move the left bound
             current_char[s[left]] -= 1
             left += 1
-
+            
+        #if the valid string is longer than existing max
         max_freq = max(right - left + 1, max_freq)
 
     return max_freq
