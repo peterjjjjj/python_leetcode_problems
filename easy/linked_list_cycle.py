06382500b1f4633b0b7b5664e9229ed64872b0e7
mengfeijin <jin.xzy@outlook.com>
2025-05-28 13:47:52 -0700

    linked_list_cycle_3

--- easy/linked_list_cycle.py
+++ easy/linked_list_cycle.py
@@ -4,6 +4,13 @@ class ListNode:
         self.next = None
 
 def has_cycle(head):
+    """
+    time complexity: O(n)
+    space complexity: O(n)
+
+    :param head:
+    :return:
+    """
     passed_nodes = set()
     #set current to the starting node
     current = head
@@ -13,4 +20,24 @@ def has_cycle(head):
             return True
         passed_nodes.add(current)
         current = current.next
+    return False
+
+def has_cycle_fast_slow(head):
+    """
+    time complexity: O(n)
+    space complexity: O(1)
+
+    :param head:
+    :return:
+    """
+    slow_pointer = head
+    fast_pointer = head.next
+
+    while fast_pointer is not None and fast_pointer.next is not None:
+        if slow_pointer == fast_pointer:
+            #they meet up, means that it is a circle
+            return True
+        #movee slow pointer up by 1, fast by 2
+        slow_pointer = slow_pointer.next
+        fast_pointer = fast_pointer.next.next
     return False
