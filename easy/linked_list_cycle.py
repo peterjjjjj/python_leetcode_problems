72110ec408fc30eb8207642589edbb2a13e7aa17
mengfeijin <jin.xzy@outlook.com>
2025-05-28 13:27:37 -0700

    linked_list_cycle_0

---
+++ easy/linked_list_cycle.py
@@ -0,0 +1,14 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def has_cycle(head):
+    passed_nodes = set()
+    while head.next is not None:
+        if head.val not in passed_nodes:
+            passed_nodes.add(head.val)
+            head = head.next
+            continue
+        return False
+    return False
