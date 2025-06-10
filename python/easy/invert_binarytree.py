b526aa793630f17da64713c9a55907ae77af115e
mengfeijin <jin.xzy@outlook.com>
2025-06-10 13:52:52 -0700

    invert_binarytree_1

--- python/easy/invert_binarytree.py
+++ python/easy/invert_binarytree.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -14,9 +16,30 @@ def invert_tree_dfs(root: TreeNode) -> TreeNode:
 
     return root
 
+def invert_tree_bfs(root: TreeNode) -> TreeNode:
+    if root is None:
+        return
+
+    queue = deque()
+    queue.append(root)
+
+    while queue:
+        current_node = queue.popleft()
+
+        if current_node.left is not None:
+            queue.append(current_node.left)
+        if current_node.right is not None:
+            queue.append(current_node.right)
+
+        current_node.left, current_node.right = current_node.right, current_node.left
+
+    return root
+
+
 if __name__ == "__main__":
     test = TreeNode(2)
     test.left = TreeNode(1)
     test.right = TreeNode(3)
     print(invert_tree_dfs(test))
+    print(invert_tree_bfs(test))
     print('1')
