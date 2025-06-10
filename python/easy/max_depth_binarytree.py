25f3bef9e873b4bcdc96d849ec5bed2c4f80afb2
mengfeijin <jin.xzy@outlook.com>
2025-06-10 10:33:07 -0700

    max_depth_binarytree_1

--- python/easy/max_depth_binarytree.py
+++ python/easy/max_depth_binarytree.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -13,6 +15,26 @@ def max_depth(root: TreeNode, depth=0) -> int:
 
     return max(left_depth, right_depth)
 
+def max_depth_bfs(root: TreeNode) -> int:
+    queue = deque()
+    queue.append(root)
+    visited = set()
+    depth = 0
+
+    while queue:
+        depth += 1
+        current_level_size = len(queue)
+
+        #clear current level
+        for _ in range(current_level_size):
+            current_node = queue.popleft()
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+
+    return depth
+
 if __name__ == "__main__":
     test = TreeNode(3)
     test.left = TreeNode(9)
@@ -20,4 +42,5 @@ if __name__ == "__main__":
     test.right.left = TreeNode(15)
     test.right.right = TreeNode(7)
     print(max_depth(test))
+    print(max_depth_bfs(test))
 
