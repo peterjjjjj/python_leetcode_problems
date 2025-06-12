6011984f3e155a9446a5baa034db3aeaad7bfb38
mengfeijin <jin.xzy@outlook.com>
2025-06-12 14:56:13 -0700

    path_sum2_1

--- python/medium/path_sum2.py
+++ python/medium/path_sum2.py
@@ -19,27 +19,33 @@ def path_sum2(root: TreeNode, target_sum: int) -> list[list[int]]:
         path.append(node.val)
 
 
-        if current_sum == target_sum:
+        if current_sum == target_sum and node.left is None and node.right is None:
             all_paths.append(path[:])
-            return
+
 
         dfs(node.left, current_sum)
         dfs(node.right, current_sum)
 
         path.pop()
 
-    dfs(root)
+    dfs(root, 0)
 
     return all_paths
 
 
 
-
-
 if __name__ == '__main__':
-    test = TreeNode(1)
-    test.left = TreeNode(2)
-    test.right = TreeNode(3)
-    print(path_sum2(test, 3))
+    test = TreeNode(5)
+    test.left = TreeNode(4)
+    test.right = TreeNode(8)
+    test.left.left = TreeNode(11)
+    test.left.left.left = TreeNode(7)
+    test.left.left.right = TreeNode(2)
+    test.right.left = TreeNode(13)
+    test.right.right = TreeNode(4)
+    test.right.right.left = TreeNode(5)
+    test.right.right.right = TreeNode(1)
+    print(path_sum2(test, 22))
+
 
 
