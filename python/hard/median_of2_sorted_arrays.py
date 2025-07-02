6bd955d2984cc5c7efaeeec0376505b2c8082f4f
mengfeijin <jin.xzy@outlook.com>
2025-07-02 15:50:24 -0700

    median_of2_sorted_3

--- python/hard/median_of2_sorted_arrays.py
+++ python/hard/median_of2_sorted_arrays.py
@@ -1,6 +1,7 @@
 def find_median_sorted_arrays(
         num1: list[int],
         num2: list[int]) -> float:
+
     if not num1:
         return num2[len(num2) //2]
     if not num2:
@@ -20,7 +21,7 @@ def find_median_sorted_arrays(
     high_boundary = len(num_short) - 1
 
     while low_boundary <= high_boundary:
-        partition_short = low_boundary + high_boundary // 2
+        partition_short = (low_boundary + high_boundary) // 2
         partition_long = total_numbers_on_left - partition_short - 1
 
         if partition_short == 0:
@@ -49,7 +50,7 @@ def find_median_sorted_arrays(
             else:
                 return max(num_short[partition_short], num_long[partition_long])
 
-        low_boundary = partition_short + high_boundary + 1 // 2
+        low_boundary = (partition_short + high_boundary + 1) // 2
 
 
     if (len(num_short) + len(num_long)) % 2 == 0:
@@ -58,6 +59,6 @@ def find_median_sorted_arrays(
         return max(num_short[partition_short], num_long[partition_long])
 
 if __name__ == '__main__':
-    num1 = []
-    num2 = [1]
+    num1 = [1,2,3,4,5]
+    num2 = [6,7,8,9,10,11,12,13,14,15,16,17]
     print(find_median_sorted_arrays(num1, num2))
