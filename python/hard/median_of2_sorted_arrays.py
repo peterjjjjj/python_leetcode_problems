ebc878fecfc907dc950374550a349537106b8760
mengfeijin <jin.xzy@outlook.com>
2025-07-02 16:58:51 -0700

    median_of2_sorted_5

--- python/hard/median_of2_sorted_arrays.py
+++ python/hard/median_of2_sorted_arrays.py
@@ -20,15 +20,15 @@ def find_median_sorted_arrays(
         num_short, num_long = num1, num2
 
     #how many numbers should be placed on the left sides
-    total_numbers_on_left = (len(num_short) + len(num_long)) // 2
+    total_numbers_on_left = (len(num_short) + len(num_long) + 1) // 2
 
     #lower and higher boundary of the shorter array
     low_boundary = 0
-    high_boundary = len(num_short) - 1
+    high_boundary = len(num_short)
 
-    while low_boundary <= high_boundary:
+    while low_boundary < high_boundary:
         partition_short = (low_boundary + high_boundary) // 2
-        partition_long = total_numbers_on_left - partition_short - 1
+        partition_long = total_numbers_on_left - partition_short
 
         if partition_short == 0:
             short_left_max = float('-inf')
@@ -40,31 +40,35 @@ def find_median_sorted_arrays(
         else:
             long_left_max = num_long[partition_long - 1]
 
-        if partition_short == len(num_short) - 1:
+        if partition_short >= len(num_short):
             short_right_min = float('inf')
         else:
-            short_right_min = num_short[partition_short + 1]
+            short_right_min = num_short[partition_short]
 
-        if partition_long == len(num_long) - 1:
+        if partition_long >= len(num_long):
             long_right_min = float('inf')
         else:
-            long_right_min = num_long[partition_long + 1]
+            long_right_min = num_long[partition_long]
 
         if short_left_max < long_right_min and long_left_max < short_right_min:
             if (len(num_short) + len(num_long)) % 2 == 0:
-                return min(num_short[partition_short], num_long[partition_long]) + max(short_left_max, long_left_max) / 2
+                return (max(short_left_max, long_left_max) + min(short_right_min, long_right_min)) / 2
             else:
-                return max(num_short[partition_short], num_long[partition_long])
-
-        low_boundary = (partition_short + high_boundary) // 2 + 1
+                return max(short_left_max, long_left_max)
 
 
+
+        low_boundary = partition_short + 1
+
+    partition_long = total_numbers_on_left - len(num_short)
     if (len(num_short) + len(num_long)) % 2 == 0:
-        return min(num_short[partition_short], num_long[partition_long]) + max(short_left_max, long_left_max) / 2
+        return (num_long[partition_long - 1] + num_long[partition_long]) / 2
     else:
-        return max(num_short[partition_short], num_long[partition_long])
+        return num_long[partition_long - 1]
 
 if __name__ == '__main__':
-    num1 = [1,2,3,4,5]
-    num2 = [6,7,8,9,10,11,12,13,14,15,16,17]
+    #num1 = [1,2,3,4,5]
+    #num2 = [6,7,8,9,10,11,12,13,14,15,16,17]
+    num1 = [3]
+    num2 = [-2,-1]
     print(find_median_sorted_arrays(num1, num2))
