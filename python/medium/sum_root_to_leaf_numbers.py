8ecab104b9569ac98b7b45f2c4719de142a05513
mengfeijin <jin.xzy@outlook.com>
2025-07-09 16:26:42 -0700

    sum_root_to_leaf_numbers_5

--- python/medium/sum_root_to_leaf_numbers.py
+++ python/medium/sum_root_to_leaf_numbers.py
@@ -12,31 +12,39 @@ def sum_root_to_leaf_numbers(root: TreeNode) -> int:
     :return:
     """
 
+    #If a tree is   1
+    #             2   3
+    #There are 2 paths, 1 -> 2, 1 ->3
+    #Sum would be 12 + 13 = 25
+
+    #List to hold all paths from root to leaf
     all_paths = []
 
-    def dfs(node: TreeNode, path: list) -> None:
+    def dfs(node: TreeNode, current_path: list) -> None:
         nonlocal all_paths
 
-        path.append(node.val)
+        #Append node value to current path
+        current_path.append(node.val)
 
+        #Node found
         if node.left is None and node.right is None:
-            all_paths.append(path[:])
+            all_paths.append(current_path[:])
             return
 
+        #If left and right node exist
         if node.left:
-            dfs(node.left, path)
-            path.pop()
+            dfs(node.left, current_path)
+            current_path.pop()
         if node.right:
-            dfs(node.right, path)
-            path.pop()
+            dfs(node.right, current_path)
+            current_path.pop()
 
     dfs(root, [])
 
     all_paths_sum = 0
 
     for path in all_paths:
-        for i in range(len(path)):
-            all_paths_sum += (10 ** (len(path) - i - 1)) * path[i]
+        all_paths_sum += int(''.join(map(str, path)))
 
 
     return all_paths_sum
