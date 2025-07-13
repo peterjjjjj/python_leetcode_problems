eef31e8d65e40b177495e65fc39f0c4a29aa535f
mengfeijin <jin.xzy@outlook.com>
2025-07-12 20:27:39 -0700

    non_overlapping_intervals_1

--- python/medium/Sorting/non_overlapping_intervals.py
+++ python/medium/Sorting/non_overlapping_intervals.py
@@ -2,7 +2,6 @@ def remove_overlapping_intervals(intervals: list[list[int]]) -> int:
     """
     Given an array of intervals where intervals[i] = [starti, endi].
     Return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
-    So only check for fully contained intervals.
 
 
     :param intervals: list[list[int]]
@@ -11,16 +10,26 @@ def remove_overlapping_intervals(intervals: list[list[int]]) -> int:
 
     remove_intervals_count = 0
 
-    #Sort the intervals by the first num in every interval.
-    intervals.sort(key=lambda x : x[0])
+    #Sort the intervals by the end i in every interval.
+    intervals.sort(key=lambda x : x[1])
 
-    for i in range(1, len(intervals)):
-        #If the starti of 2 intervals were overlapped.
-        if intervals[i][0] <= intervals[i - 1][0]:
-            #Increase count.
+    #Keep track of the last interval's end time.
+    previous_end = intervals[0][1]
+
+    i = 1
+    while i < len(intervals):
+        #The end time is sorted, if start time of current is less than previous.
+        #Means there is an overlap in those 2 intervals.
+        if intervals[i][0] < previous_end:
             remove_intervals_count += 1
+        else:
+            previous_end = intervals[i][1]
+
+        i += 1
+
+    return remove_intervals_count
 
 
 if __name__ == '__main__':
-    pass
+    print(remove_overlapping_intervals([[1,100],[11,22],[1,11],[2,12]]))
 
