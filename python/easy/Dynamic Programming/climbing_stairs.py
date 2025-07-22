442303ddb32c59774a1e87a593619ddf4b53d4f3
mengfeijin <jin.xzy@outlook.com>
2025-07-22 11:09:40 -0700

    climbing_stairs_0

---
+++ python/easy/Dynamic Programming/climbing_stairs.py
@@ -0,0 +1,22 @@
+def climb_stairs(n: int) -> int:
+    """
+    You are climbing a staircase, takes n steps to reach the top.
+    Each time you climb 1 or 2 steps, how many ways can u climb to the top.
+
+    :param n: int
+    return: int
+    """
+
+    dp = []
+    dp.append(1)
+    dp.append(2)
+
+    for i in range(3, n+1):
+        dp.append(
+            dp[i-2] + dp[i-3]
+        )
+
+    return dp[-1]
+
+if __name__ == '__main__':
+    print(climb_stairs(3))
