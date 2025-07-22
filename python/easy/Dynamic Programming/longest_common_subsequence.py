88a1232bd6328e388d2a1a9f6a72efca36e6bfcb
mengfeijin <jin.xzy@outlook.com>
2025-07-22 11:58:57 -0700

    longest_common_subsequence_1
    Not much idea, asked GPT, wrote but not understood.

--- python/easy/Dynamic Programming/longest_common_subsequence.py
+++ python/easy/Dynamic Programming/longest_common_subsequence.py
@@ -5,7 +5,7 @@ def longest_common_subsequence(text1: str, text2: str) -> int:
     for i in range(1, len(text1) + 1):
         for j in range(1, len(text2) + 1):
             if text1[i - 1] == text2[j - 1]:
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + 1
+                dp[i][j] = dp[i-1][j-1] + 1
             else:
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
