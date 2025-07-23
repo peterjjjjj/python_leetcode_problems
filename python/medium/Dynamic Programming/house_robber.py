7b9b779f407f511b946f3d51f7a7ec127f28e495
mengfeijin <jin.xzy@outlook.com>
2025-07-23 12:02:42 -0700

    house_robber_0

--- python/medium/Dynamic Programming/house_robber.py
+++ python/medium/Dynamic Programming/house_robber.py
@@ -6,11 +6,15 @@ def rob(nums: list[int]) -> int:
     :return: int
     """
 
+    #Initialize the dp table.
     dp = [0 for _ in range(len(nums) + 1)]
     dp[1] = nums[0]
 
     for i in range(1, len(nums)):
+        #The index in the dp table.
         dp_index = i + 1
+        #Cuz you cannot rob 2 adjacent houses, either you rob the previous house(dp[dp_index - 1] or this house.
+        #Rubbing this house means you get to rub house before the previous house which is nums[i] + dp[dp_index - 2].
         dp[dp_index] = max(nums[i] + dp[dp_index - 2], dp[dp_index - 1])
 
     return dp[-1]
