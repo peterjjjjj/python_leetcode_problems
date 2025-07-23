cac90cb664e3a8672b4e563d51e5b5efef85b9df
mengfeijin <jin.xzy@outlook.com>
2025-07-23 13:33:17 -0700

    assign_cookies_0

---
+++ python/easy/Greedy/assign_cookies.py
@@ -0,0 +1,20 @@
+def max_cookies(g: list[int], s: list[int]) -> int:
+    """
+
+    :param g: list[int]
+    :param s: list[int]
+    :return: int
+    """
+
+    i = 0
+    count = 0
+
+    for j in range(len(g)):
+        if s[i] >= g[j]:
+            count += 1
+            i += 1
+
+    return count
+
+if __name__ == "__main__":
+    print(max_cookies([1,2,3], [1,1]))
