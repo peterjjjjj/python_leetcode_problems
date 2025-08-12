93c63ac821228c4cf3c40f0d926fda34c6fb664c
mengfeijin <jin.xzy@outlook.com>
2025-08-12 16:19:19 -0700

    house_robber_2_6

--- python/medium/Dynamic Programming/house_robber_2.py
+++ python/medium/Dynamic Programming/house_robber_2.py
@@ -25,6 +25,36 @@ def rob(nums: list[int]) -> int:
 
     return max(first_house_dp[-1], last_house_dp[-1])
 
+def rob_2(nums: list[int]) -> int:
+
+    n = len(nums)
+    if n == 0:
+        return 0
+    if n == 1:
+        return nums[0]
+
+    def rob_linear(nums: list[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        dp = [0 for _ in range(len(nums) + 1)]
+        dp[1] = nums[0]
+
+        for i in range(2, len(nums) + 1):
+            dp[i] = max(dp[i - 1], nums[i - 1] + dp[i - 2])
+
+        return dp[-1]
+
+    #The robbing logic is the same as robbing house 1.
+    #The first scenario, rob first not last, is nums[:length - 1].
+    #Second scenario, rob last not first, is nums[1:].
+    rob_first = rob_linear(nums[:n - 1])
+    rob_last = rob_linear(nums[1:])
+
+    return max(rob_first, rob_last)
+
 if '__main__' == __name__:
     print(rob([2,3,2]))
     print(rob([1,1]))
+    print(rob_2([2,3,2]))
+    print(rob_2([1,1]))
