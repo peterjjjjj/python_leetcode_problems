014bb9a66ca44f52c2c99dfc098ce9aa6a6a3c5d
mengfeijin <jin.xzy@outlook.com>
2025-08-12 15:49:36 -0700

    house_robber_2_3

--- python/medium/Dynamic Programming/house_robber_2.py
+++ python/medium/Dynamic Programming/house_robber_2.py
@@ -3,27 +3,19 @@ def rob(nums: list[int]) -> int:
     LC 213.
     """
 
-    if len(nums) == 1:
-        return nums[0]
 
     #Construct the dp table.
-    dp = [0 for _ in range(len(nums) + 1)]
+    dp = [0 for _ in range(len(nums))]
 
-    if len(nums) % 2 == 0:
-        dp[1] = nums[0]
+    dp[1] = max(nums[0], nums[-1])
 
-    for i in range(2, len(nums)):
-        dp[i] = max(dp[i - 1], nums[i - 1] + dp[i - 2])
+    for i in range(1, len(nums)):
+        dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1])
 
-
-    if len(nums) % 2 == 1:
-        dp[len(nums)] = max(dp[len(nums) - 1], max(nums[len(nums) - 1], nums[0]) + dp[len(nums) - 2])
-
-    else:
-        dp[len(nums)] = max(dp[len(nums) - 1], nums[len(nums) - 1] + dp[len(nums) - 2])
 
     return dp[-1]
 
 if '__main__' == __name__:
     print(rob([2,3,2]))
     print(rob([1,2,3,1]))
+    print(rob([2,1,1,2]))
