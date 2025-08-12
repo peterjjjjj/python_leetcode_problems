32855eb2cc0dd338c00bbc0217ce5220f041b332
mengfeijin <jin.xzy@outlook.com>
2025-08-12 10:28:19 -0700

    unique_paths_2_0

--- python/medium/Dynamic Programming/unique_paths_2.py
+++ python/medium/Dynamic Programming/unique_paths_2.py
@@ -6,6 +6,8 @@ def unique_paths(obstacleGrid: list[list[int]]) -> int:
     #Initialize the dp table.
     dp = [[0 for _ in range(len(obstacleGrid[0]))] for _ in range(len(obstacleGrid))]
 
+    #Initialize the first row and column.
+    #If obstacle encountered, break that loop.
     for i in range(len(obstacleGrid)):
         if obstacleGrid[i][0] == 1:
             break
@@ -18,6 +20,7 @@ def unique_paths(obstacleGrid: list[list[int]]) -> int:
 
         dp[0][i] = 1
 
+    #Same logic as Unique Path 1.
     for i in range(1, len(obstacleGrid)):
         for j in range(1, len(obstacleGrid[0])):
             if obstacleGrid[i][j] == 1:
