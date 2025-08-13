1d23c5b35468f5106c38abdbe85a8c2a8f1cb84f
mengfeijin <jin.xzy@outlook.com>
2025-08-13 14:06:39 -0700

    binary_tree_max_path_sum_0

--- python/hard/Trees/binary_tree_max_path_sum.py
+++ python/hard/Trees/binary_tree_max_path_sum.py
@@ -9,25 +9,43 @@ def max_path_sum(root: TreeNode) -> int:
     LC 124.
     """
 
-    sum = 0
+    #Initialize the dp table.
+    dp = [0]
+    i = 0
+    max_sum = 0
 
     def dfs(node: TreeNode) -> None:
-        nonlocal sum
+        nonlocal max_sum, dp, i
+
         if node is None:
             return
 
-        sum += node.val
-
+        #Use inorder to make sure that all the nodes are linked.
         dfs(node.left)
+
+        #Append an 0 to the dp list.
+        dp.append(0)
+        #Increment index.
+        i += 1
+
+        #If including the current node would make the path larger.
+        #Update the max_sum and dp list.
+        if dp[i - 1] + node.val >+ max_sum:
+            max_sum = dp[i - 1] + node.val
+            dp[i] = max_sum
+
         dfs(node.right)
 
     dfs(root)
 
-    return sum
+    return max_sum
+
 
 
 if __name__ == '__main__':
-    tree = TreeNode(1)
-    tree.left = TreeNode(2)
-    tree.right = TreeNode(3)
+    tree = TreeNode(-10)
+    tree.left = TreeNode(9)
+    tree.right = TreeNode(20)
+    tree.right.left = TreeNode(15)
+    tree.right.right = TreeNode(7)
     print(max_path_sum(tree))
