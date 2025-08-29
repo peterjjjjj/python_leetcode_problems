8533e8c2375e30845e351fe358ccd835f9a1f87b
mengfeijin <jin.xzy@outlook.com>
2025-08-29 15:11:39 -0700

    different_way_add_parenthesis_unfinished

--- python/medium/Dynamic Programming/different_way_add_parenthesis.py
+++ python/medium/Dynamic Programming/different_way_add_parenthesis.py
@@ -35,6 +35,29 @@ def ways_to_compute_recursion(expression: str) -> list[int]:
 
     return compute(expression)
 
+def ways_to_compute_dp(expression: str) -> list[int]:
+
+    nums = []
+    signs = []
+
+    temp = ''
+
+    for i in range(len(expression)):
+        if expression[i].isdigit():
+            temp += expression[i]
+        else:
+            signs.append(expression[i])
+            nums.append(int(temp))
+            temp = ''
+
+    dp = [[[] for _ in range(len(nums))] for _ in range(len(nums))]
+
+    for i in range(len(nums)):
+        dp[i][i] = [nums[i]]
+
+    pass
+
+
 
 if __name__ == '__main__':
     #print(ways_to_compute_recursion('1+2*3'))
