67aa3a1ecd5b6b85fb6c0522d5187f13eaa75fb0
mengfeijin <jin.xzy@outlook.com>
2025-10-20 12:17:55 -0700

    sum2 review

--- python/medium/Array Hashing/sum2.py
+++ python/medium/Array Hashing/sum2.py
@@ -28,27 +28,28 @@ def twoSum2(nums, target):
     return False
 
 
-def twoSumReview(nums: list[int], target: int) -> list[int]:
+def twoSumHash(nums: list[int], target: int) -> list[int]:
     """
     Review of sum2.
+    TC, SC: O(n)
     """
 
-    #The structure of the hash is reversed because we are asking for the indices, {complement value : num index}.
+    #The structure of the hash is reversed because we are asking for the indices, {previous num : index}.
     hash_table = {}  # Dict cuz of time complexity.
 
-    for i in range(len(nums)):
+    for i, num in enumerate(nums):
         complement = target - nums[i]
 
         # If num could be one of the complements.
-        if nums[i] in hash_table:
-            return [hash_table[nums[i]], i]
+        if complement in hash_table:
+            return [hash_table[complement], i]
 
-        hash_table[nums[i] - target] = i  # Key: complement of num, Value: index.
+        hash_table[num] = i  # Key: num, Value: index.
 
     return []
 
 
 nums = [-1,0,1,2,-1,-4]
-print(twoSum(nums, 0))
-print(twoSum2(nums, 6))
-print(twoSumReview(nums, 6))
+#print(twoSum(nums, 0))
+#print(twoSum2(nums, 6))
+print(twoSumHash([2, 7, 11, 15], 9))
