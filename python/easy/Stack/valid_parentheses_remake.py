65ccf9f4297e6c412cba2a236ed75366f87b9ddb
mengfeijin <jin.xzy@outlook.com>
2025-12-26 14:40:53 -0800

    valid_parentheses_remake.py

--- python/easy/Stack/valid_parentheses_remake.py
+++ python/easy/Stack/valid_parentheses_remake.py
@@ -1,4 +1,6 @@
 def valid_parentheses(s: str) -> bool:
+    if len(s) == 0:
+        return True
     stack = []
     pair = {'(' : ')', '[' : ']', '{' : '}'}
 
@@ -6,6 +8,8 @@ def valid_parentheses(s: str) -> bool:
         if char in ['(', '[', '{']:
             stack.append(char)
         elif char in [')', ']', '}']:
+            if not stack:
+                return False
             if char != pair[stack.pop()]:
                 return False
 
