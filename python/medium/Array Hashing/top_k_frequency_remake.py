13479c021f229bbaa25366d93976d6806a5f0076
mengfeijin <jin.xzy@outlook.com>
2026-01-16 14:36:18 -0800

    top_k_frequency_remake.py

--- python/medium/Array Hashing/top_k_frequency_remake.py
+++ python/medium/Array Hashing/top_k_frequency_remake.py
@@ -32,6 +32,28 @@ def heap(nums: list[int], k: int) -> list[int]:
     return results
 
 
+def bucket_sort(nums: list[int], k: int) -> list[int]:
+    #Get the count of nums
+    counts = collections.Counter(nums)
+
+    #Initialize empty list
+    bucket = [[] for _ in range(len(nums) + 1)]
+
+    for num, count in counts.items():
+        #Key in the nums with its count in the bucket list
+        bucket[count].append(num)
+
+    results = []
+
+    for i in range(len(bucket) - 1, 0, -1):
+        if bucket[i]:
+            for num in bucket[i]:
+                results.append(num)
+            if len(results) == k:
+                return results
+
+
 if __name__ == '__main__':
     print(sorting([1,1,1,2,2,0,0,0,0,0,3], 2))
     print(heap([1,1,1,2,2,0,0,0,0,0,0,4], 2))
+    print(bucket_sort([1,1,1,2,2,0,0,0,0,0,0,4], 2))
